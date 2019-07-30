888459cee7f3b35f47bd0073a9905ea2295dffc1
CongPhan95 <47944741+CongPhan95@users.noreply.github.com>
2019-07-30 08:29:12 +0700

    C

--- itopw_api/routes/api.php
+++ itopw_api/routes/api.php
@@ -16,3 +16,23 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/serie', 'SerieController@index')->name('serie-index');
+Route::get('/serie/{serie}', 'SerieController@show')->name('serie-show');
+Route::post('/serie', 'SerieController@store')->name('serie-store');
+Route::put('/serie/{serie}', 'SerieController@update')->name('serie-update');
+Route::delete('/serie/{serie}', 'SerieController@delete')->name('serie-delete');
+
+Route::get('/category', 'CategoryController@index')->name('category-index');
+Route::get('/category/{category}', 'CategoryController@show');
+Route::post('/category', 'CategoryController@store')->name('category-store');
+Route::put('/category/{category}', 'CategoryController@update')->name('category-update');
+Route::delete('/category/{category}', 'CategoryController@delete')->name('category-delete');
+
+Route::get('/post', 'PostController@index')->name('post-index');
+Route::get('/post/{post}', 'PostController@show')->name('post-show');
+Route::post('/post', 'PostController@store')->name('post-store');
+Route::put('/post/{post}', 'PostController@update')->name('post-update');
+Route::delete('/post/{post}', 'PostController@delete')->name('post-delete');
+
+
