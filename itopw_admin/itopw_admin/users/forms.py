2ba7d532590b1cc7d3bb329d58a5e0bdf4efc149
son4ln <son4ln@gmail.com>
2019-08-03 16:24:46 +0700

    update docker compose

--- itopw_admin/itopw_admin/users/forms.py
+++ itopw_admin/itopw_admin/users/forms.py
@@ -1,30 +1,25 @@
-from django.contrib.auth import get_user_model, forms
+from django.contrib.auth import get_user_model
+from django.forms import ModelForm, TextInput, EmailInput
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 User = get_user_model()
 
 
-class UserChangeForm(forms.UserChangeForm):
-    class Meta(forms.UserChangeForm.Meta):
+class SuperUpdateUserForm(ModelForm):
+    """Form definition for MODELNAME."""
+
+    class Meta:
+        """Meta definition for MODELNAMEform."""
+
         model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'is_staff', 'is_superuser', 'is_writter', 'phone',
+            'email_notification', 'web_notification', 'name')
 
-
-class UserCreationForm(forms.UserCreationForm):
-
-    error_message = forms.UserCreationForm.error_messages.update(
-        {"duplicate_username": _("This username has already been taken.")}
-    )
-
-    class Meta(forms.UserCreationForm.Meta):
-        model = User
-
-    def clean_username(self):
-        username = self.cleaned_data["username"]
-
-        try:
-            User.objects.get(username=username)
-        except User.DoesNotExist:
-            return username
-
-        raise ValidationError(self.error_messages["duplicate_username"])
+        widgets = {
+            "first_name": TextInput(attrs={"class": "form-control", "placeholder": _("Your First Name")}),
+            "last_name": TextInput(attrs={"class": "form-control", "placeholder": _("Your Last Name")}),
+            "name": TextInput(attrs={"class": "form-control", "placeholder": _("Your Full Name")}),
+        }
